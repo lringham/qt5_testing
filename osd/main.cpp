b6a098e2e021af8bb3f4e780350954921e736f76
Lee <lee.ringham@gmail.com>
2025-12-20 14:32:18 -0700

    add vcpkg and gitignore

--- osd/main.cpp
+++ osd/main.cpp
@@ -1,8 +1,10 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
+#include <fmt/core.h>
 
 int main(int argc, char* argv[])
 {
+	fmt::print("Hello, World!");
 	QApplication app(argc, argv);
 	QLabel* label = new QLabel("Hello Qt!");
 	label->show();
