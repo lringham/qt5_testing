533260dea77b29bd2b2ea3b1aba5de05b064e2a9
Lee <lee.ringham@gmail.com>
2025-12-22 15:12:01 -0700

    add more modules, try include QPointF

--- osd/main.cpp
+++ osd/main.cpp
@@ -1,12 +1,13 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
+#include "QPointF"
 #include <fmt/core.h>
 
 int main(int argc, char* argv[])
 {
 	fmt::print("Hello, World!");
 	QApplication app(argc, argv);
-	QLabel* label = new QLabel("Hello Qt!");
-	label->show();
+	QPointF point(10.0, 20.0);
+	fmt::print("Point coordinates: ({}, {})\n", point.x(), point.y());
 	return app.exec();
 }
